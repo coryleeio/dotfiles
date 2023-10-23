a7fc285249b7cd47bfeb94340a2455e58928e58a
Cory Lee <cl10001123@gmail.com>
2023-10-22 20:24:43 -0400

    Iterating...

--- .config/nvim/lua/coryl/plugins/telescope.lua
+++ .config/nvim/lua/coryl/plugins/telescope.lua
@@ -8,7 +8,15 @@ return {
   },
   config = function()
     local telescope = require("telescope")
+    local transform_mod = require('telescope.actions.mt').transform_mod
     local actions = require("telescope.actions")
+    local mod = {}
+    mod.open_in_nvim_tree = function(prompt_bufnr)
+      local cur_win = vim.api.nvim_get_current_win()
+      vim.cmd("NvimTreeFindFile")
+      vim.api.nvim_set_current_win(cur_win)
+    end
+    mod = transform_mod(mod)
 
     telescope.setup({
       defaults = {
@@ -18,9 +26,15 @@ return {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            ["<CR>"] = actions.select_default + mod.open_in_nvim_tree,
+          },
+
+          n = {
+            ["<CR>"] = actions.select_default + mod.open_in_nvim_tree,
           },
         },
       },
+    },
     })
 
     telescope.load_extension("fzf")
