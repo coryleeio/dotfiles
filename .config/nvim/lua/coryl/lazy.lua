cc4d507334ac785680eaf9d2ac432d3f306581e0
Cory Lee <cl10001123@gmail.com>
2023-10-22 21:11:53 -0400

    Iterating...

--- .config/nvim/lua/coryl/lazy.lua
+++ .config/nvim/lua/coryl/lazy.lua
@@ -11,5 +11,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({{import = "coryl.plugins"}, {import = "coryl.plugins.lsp"})
+require("lazy").setup({import = "coryl.plugins"}, {import = "coryl.plugins.lsp"})
 
