c5540b72a5219c060ded910f9b0d687c948ee534
Cory Lee <cl10001123@gmail.com>
2023-11-04 14:28:23 -0400

    x

--- .config/nvim/lua/coryl/plugins/nvim-cmp.lua
+++ .config/nvim/lua/coryl/plugins/nvim-cmp.lua
@@ -39,7 +39,8 @@ return {
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
-        { name = "nvim_lsp" },
+        { name = "nvim_lsp" }, -- language lsp
+        { name = "copilot" }, -- copilot
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
