ebb0e8bdb1b19512cc436b35b3fcebbf8a0e74d2
Cory Lee <cl10001123@gmail.com>
2023-11-10 09:40:58 -0500

    x

--- .config/nvim/lua/coryl/plugins/telescope.lua
+++ .config/nvim/lua/coryl/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
   },
   config = function()
     local telescope = require("telescope")
-    local transform_mod = require('telescope.actions.mt').transform_mod
+    local transform_mod = require("telescope.actions.mt").transform_mod
     local actions = require("telescope.actions")
     local mod = {}
     mod.open_in_nvim_tree = function(prompt_bufnr)
@@ -45,5 +45,6 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fm", "<cmd>Telescope keymaps<cr>", { desc = "Find vim keymaps" })
   end,
 }
