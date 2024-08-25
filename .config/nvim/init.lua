177b9c93490478bdb3706ca06d0803b5451fb727
Cory Lee <cl10001123@gmail.com>
2024-08-25 15:17:52 -0400

    Switch to starship theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -818,6 +818,7 @@ require('lazy').setup({
   --
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'custom.plugins.obsidian' },
+  { import = 'custom.plugins.tmux-navigator' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
