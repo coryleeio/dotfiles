da284d51f987a798c6140063940277516847d801
Cory Lee <cl10001123@gmail.com>
2025-05-23 10:45:18 -0400

    Iterating...

--- config/nvim2/lua/custom/plugins/auto-session.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'rmagatti/auto-session',
-  config = function()
-    require("auto-session").setup {
-      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-    }
-  end
-}
