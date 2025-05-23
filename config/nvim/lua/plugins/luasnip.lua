91e14a70e05066401be7506422e28a43a9307375
Cory Lee <cl10001123@gmail.com>
2025-05-23 12:02:02 -0400

    Iterating...

---
+++ config/nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,32 @@
+return {
+  "L3MON4D3/LuaSnip",
+  lazy = true,
+  build = (not LazyVim.is_win())
+      and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+    or nil,
+  dependencies = {
+    {
+      "coryleeio/snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+      end,
+    },
+  },
+  opts = function()
+    LazyVim.cmp.actions.snippet_forward = function()
+      if require("luasnip").jumpable(1) then
+        vim.schedule(function()
+          require("luasnip").jump(1)
+        end)
+        return true
+      end
+    end
+    LazyVim.cmp.actions.snippet_stop = function()
+      if require("luasnip").expand_or_jumpable() then -- or just jumpable(1) is fine?
+        require("luasnip").unlink_current()
+        return true
+      end
+    end
+  end,
+}
