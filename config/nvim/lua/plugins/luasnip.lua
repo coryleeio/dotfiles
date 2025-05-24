bfbc2f2210b2aa8b51fd625fd613bcd530f9b6fa
Cory Lee <cl10001123@gmail.com>
2025-05-23 20:20:38 -0400

    Iterating...

--- config/nvim/lua/plugins/luasnip.lua
+++ config/nvim/lua/plugins/luasnip.lua
@@ -7,6 +7,7 @@ return {
   dependencies = {
     {
       "coryleeio/snippets",
+      version = false, -- always grab latest commit
       config = function()
         require("luasnip.loaders.from_vscode").lazy_load()
         require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
