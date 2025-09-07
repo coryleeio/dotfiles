8d32d64176067e1c148bf2ae25cc5e0f58ee525b
Cory Lee <cl10001123@gmail.com>
2025-09-06 21:43:50 -0400

    space sf should search for files

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Add <leader>sf to search files (same as <leader><space>)
+vim.keymap.set("n", "<leader>sf", require("lazyvim.util").pick("files"), { desc = "Find Files" })
